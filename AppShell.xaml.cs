742f65b42671bf54faae425e800b631dedf3cb2f
hynnah <hannahmarie.martinez.12@gmail.com>
2026-04-26 21:29:00 +0800

    MODIFY: appshell.xaml.cs

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,20 +1,17 @@
-﻿namespace Shutool.Views; // NO "public" here!
+﻿namespace Shutool;
+using Shutool.Views;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
-        //InitializeComponent();
+        InitializeComponent();
+
+        // Register routes using the proper namespace, with 'using Shutool.Views' added
         Routing.RegisterRoute("SignUpPage", typeof(SignUpPage));
-
         Routing.RegisterRoute("RiderHistoryPage", typeof(RiderHistoryPage));
-
         Routing.RegisterRoute("DriverHistoryPage", typeof(DriverHistoryPage));
-
-        Routing.RegisterRoute("ProfilePage", typeof(Views.ProfilePage));
-
-        Routing.RegisterRoute("AdminRequestLogPage", typeof(Views.AdminRequestLogPage));
+        Routing.RegisterRoute("ProfilePage", typeof(ProfilePage)); // <-- No need to prefix with Views
+        Routing.RegisterRoute("AdminRequestLogPage", typeof(AdminRequestLogPage)); // <-- No need to prefix with Views
     }
 }
-
-//test
